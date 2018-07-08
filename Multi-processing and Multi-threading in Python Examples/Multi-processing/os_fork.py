3d026f85ff343b3eec1aa886a6ebd8a7005196c6
Ziang Lu <Ziang-Lu@users.noreply.github.com>
2018-07-08 01:57:24 -0700

    reformat coding style

--- Multi-processing and Multi-threading in Python Examples/Multi-processing/os_fork.py
+++ Multi-processing and Multi-threading in Python Examples/Multi-processing/os_fork.py
@@ -15,10 +15,13 @@ def main():
     pid = os.fork()  # 操作系统把当前进程(父进程)复制一份(称为子进程)
     # os.fork()函数调用一次, 返回两次: 父进程中返回创建的子进程的ID, 子进程中返回0
     if pid != 0:
-        print('I (%d) just created a child process (%d).' % (os.getpid(), pid))
+        print('I ({pid}) just created a child process {child_id}.'.format(
+            pid=os.getpid(), child_id=pid
+        ))
     else:
-        print('I am child process (%d) and my parent is %d.' %
-              (os.getpid(), os.getppid()))
+        print('I am child process ({pid}) and my parent is {parent_id}.'.format(
+            pid=os.getpid(), parent_id=os.getppid()
+        ))
 
 
 if __name__ == '__main__':
